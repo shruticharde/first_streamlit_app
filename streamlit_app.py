80a03d3f8c69e9bdc91e179a22c4803fbd4f1e50
Shruti Charde <55059543+shruticharde@users.noreply.github.com>
2022-07-12 18:22:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
 #Create the repeat block
-def get_fruityvice_data(the fruit choice):
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return (fruityvice_normalized
