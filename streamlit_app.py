33f9ac3f9d0c710d749370f611821f929787c856
Shruti Charde <55059543+shruticharde@users.noreply.github.com>
2022-07-12 13:00:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,5 @@ import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
